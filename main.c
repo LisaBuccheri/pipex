c812adab522681dedb5e50c52dec0cb44a9a11fc
LisaBuccheri <lisa.buccheri@hotmail.com>
2022-01-12 10:57:53 +0100

    last changes
    add message error when argument != 5 and when fd of the out file has an error

--- main.c
+++ main.c
@@ -103,11 +103,17 @@ int	main(int argc, char *argv[], char *envp[])
 	t_fd	fds;
 
 	if (argc != 5)
+	{
+		perror(strerror(22));
 		exit(1);
+	}
 	fds.f_in = open(argv[1], O_RDONLY);
 	fds.f_out = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (fds.f_out < 0)
+	{
+		perror(strerror(9));
 		exit(127);
+	}
 	pipex(fds, argv, envp);
 	close(fds.f_in);
 	close(fds.f_out);
